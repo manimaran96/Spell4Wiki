7d8291fcedf454d5fc0b50c003df79ad2059de5e
Manimaran <manimaraninam1027@gmail.com>
2018-11-28 00:52:40 +0530

    Init commit

---
+++ app/src/main/java/com/manimaran/wikiaudio/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.manimaran.wikiaudio;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Splash screen activity
+ */
+public class SplashActivity extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 1500; //splash screen will be shown for 1.5 seconds
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Todo : If login go MainActivity else Ask for login
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
