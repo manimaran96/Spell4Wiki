e0aeff04ea2c6e78fa05f176900d7dd52e16cbf2
Manimaran.K <manimaraninam1027@gmail.com>
2020-01-06 01:40:27 +0530

    spell4wordlist functionality done

--- app/src/main/java/com/manimaran/wikiaudio/activity/UploadToCommonsActivity.java
+++ app/src/main/java/com/manimaran/wikiaudio/activity/UploadToCommonsActivity.java
@@ -9,12 +9,17 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.manimaran.wikiaudio.R;
 import com.manimaran.wikiaudio.adapter.EndlessAdapter;
+import com.manimaran.wikiaudio.utils.GeneralUtils;
+import com.manimaran.wikiaudio.utils.RealPathUtil;
 import com.manimaran.wikiaudio.view.EndlessListView;
 
 import java.io.BufferedReader;
@@ -22,6 +27,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -31,6 +37,10 @@ public class UploadToCommonsActivity extends AppCompatActivity {
     private static final int EDIT_REQUEST_CODE = 42;
 
 
+    private Button btnSelectFile, btnDone;
+    private EditText editFile;
+    private TextView txtFileInfo;
+    private View layoutEdit, layoutSelect;
     private EndlessListView resultListView;
     private EndlessAdapter adapter;
 
@@ -39,24 +49,43 @@ public class UploadToCommonsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_to_commons);
 
+        initUI();
+
+
+    }
+
+    private void initUI() {
+
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setTitle(getString(R.string.spell4wordlist));
         }
 
-        Button btnPickFile = findViewById(R.id.btn_pick_file);
+        btnSelectFile = findViewById(R.id.btnSelectFile);
+        btnDone = findViewById(R.id.btnDone);
+        editFile = findViewById(R.id.editFile);
+        txtFileInfo = findViewById(R.id.txtFileInfo);
+        resultListView = findViewById(R.id.listView);
+        layoutSelect = findViewById(R.id.layoutSelect);
+        layoutEdit = findViewById(R.id.layoutEdit);
 
-        btnPickFile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                    editDocument();
-                }
+
+        btnSelectFile.setOnClickListener(v -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                editDocument();
             }
         });
 
-        resultListView = findViewById(R.id.search_result_list);
+        btnDone.setOnClickListener(v -> {
+            //svf
+        });
+
         resultListView.setLoadingView(R.layout.loading_row);
+
+        layoutSelect.setVisibility(View.VISIBLE);
+        layoutEdit.setVisibility(View.GONE);
+        resultListView.setVisibility(View.GONE);
+
     }
 
 
@@ -96,27 +125,128 @@ public class UploadToCommonsActivity extends AppCompatActivity {
             if (resultData != null) {
                 uri = resultData.getData();
                 assert uri != null;
-                File file = new File(getRealPathFromURI(uri));
+                File file = new File(RealPathUtil.getRealPath(getApplicationContext(), uri));
                 String TAG = "UploadToCommonsActivity";
-                adapter = new EndlessAdapter(this, readFromFile(file.getPath()), R.layout.search_result_row, true);
-                resultListView.setAdapter(adapter);
-                resultListView.setVisibility(View.VISIBLE);
+                openFileInAlignMode(file.getAbsolutePath(), file.getName());
+
             }
         }
     }
 
-    private String getRealPathFromURI(Uri contentURI) {
-        String result;
-        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
-        if (cursor == null) { // Source is Dropbox or other similar local file path
-            result = contentURI.getPath();
-        } else {
-            cursor.moveToFirst();
-            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-            result = cursor.getString(idx);
-            cursor.close();
+    private void openFileInAlignMode(String filePath, String fileName) {
+
+
+        layoutSelect.setVisibility(View.GONE);
+        layoutEdit.setVisibility(View.VISIBLE);
+        resultListView.setVisibility(View.GONE);
+
+        txtFileInfo.setText(("Align the file \n" + fileName));
+        editFile.setText(getContentFromFile(filePath));
+
+
+        btnDone.setOnClickListener(v -> {
+            if(!TextUtils.isEmpty(editFile.getText())) {
+                List<String> items = getWordListFromString(editFile.getText().toString());
+                showWordsInRecordMode(items);
+            }
+            else
+                GeneralUtils.showSnack(editFile, "Check content");
+
+        });
+
+
+
+    }
+
+    private void showWordsInRecordMode(List<String> items) {
+
+        layoutSelect.setVisibility(View.GONE);
+        layoutEdit.setVisibility(View.GONE);
+        resultListView.setVisibility(View.VISIBLE);
+
+
+        adapter = new EndlessAdapter(this, items, R.layout.search_result_row, true);
+        resultListView.setAdapter(adapter);
+        resultListView.setVisibility(View.VISIBLE);
+    }
+
+    private List<String> getWordListFromString(String data) {
+
+        List<String> list = new ArrayList<>();
+        try {
+            if(data != null && data.length() > 0) {
+                String[] l = data.split("\n");
+
+                if(l.length > 0){
+                    for(String s : l){
+                        if(s != null)
+                        {
+                            String word = s.trim();
+                            if(word.length() > 0){
+                                list.add(word);
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        return list;
+    }
+
+    private String getRealPathFromURI(Uri contentURI) {
+        String result = null;
+        Cursor cursor = null;//ww  w.j  a  va2  s. c om
+        try {
+            String[] proj = { MediaStore.Images.Media.DATA };
+            cursor = getContentResolver().query(contentURI, proj,
+                    null, null, null);
+            int column_index = cursor != null ? cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA) : 0;
+            if(cursor != null && cursor.moveToFirst())
+                result = cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+
         return result;
+    }
+
+    private String getContentFromFile(String fileName){
+        String data = "";
+        try {
+            FileInputStream inputStream = new FileInputStream(fileName);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+
+            String line;
+
+            // Reading line by line from the
+            // file until a null is returned
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+
+            inputStream.close();
+            data = stringBuilder.toString().trim();
+
+            /*while ((line = reader.readLine()) != null) {
+                String s = line.trim();
+                if (s.length() > 0)
+                    list.add(line);
+            }*/
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return data;
     }
 
     private List<String> readFromFile(String fileName) {
