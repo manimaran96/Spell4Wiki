05ca407120a24a7f41316191ba9ff44f1cfdf2d1
Manimaran <manimaraninam1027@gmail.com>
2020-06-12 23:52:07 +0530

    network utils added

--- app/src/main/java/com/manimarank/spell4wiki/utils/NetworkUtils.kt
+++ app/src/main/java/com/manimarank/spell4wiki/utils/NetworkUtils.kt
@@ -1,4 +1,37 @@
 package com.manimarank.spell4wiki.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
 object NetworkUtils {
+
+    fun isConnected(context: Context): Boolean {
+        var result = false
+        val connectivityManager =
+                context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val networkCapabilities = connectivityManager.activeNetwork ?: return false
+            val actNw =
+                    connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+            result = when {
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                else -> false
+            }
+        } else {
+            connectivityManager.run {
+                connectivityManager.activeNetworkInfo?.run {
+                    result = when (type) {
+                        ConnectivityManager.TYPE_WIFI -> true
+                        ConnectivityManager.TYPE_MOBILE -> true
+                        else -> false
+                    }
+
+                }
+            }
+        }
+        return result
+    }
 }
